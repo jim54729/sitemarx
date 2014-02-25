21cd5d55ff61fba47451fd7c907724874a57bf9e
Jim Schwetz <jim54729@gmail.com>
2014-02-24 19:48:27 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sitemarx::Application.config.secret_key_base = 'e9707284334bb4f0c2b2e393d8601af4b265868f5d290a8fd39c7051918a8618210ab003541c7766a7a392751833dfb95b738682f0072564ecd225df6abc1b4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sitemarx::Application.config.session_store :cookie_store, key: '_sitemarx_session'
